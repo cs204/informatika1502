389788234979768831ec2d35e4fe171e6d5fbaf2
test <test>
2023-03-12 20:46:48 +0700

    test

---
+++ extensions.py
@@ -0,0 +1,11 @@
+x = str(input("File name:"))
+
+if x == "fig.png":
+    print("image/png")
+
+elif x == "hello.jpg":
+    print(" image/jpeg")
+
+elif x == " document.pdf":
+    print("application/pdf")
+
